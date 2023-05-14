2bb752e3b5029044ed000d16d03bc1c4be9f3679
areeb <areebqzi@gmail.com>
2023-05-14 13:47:30 +0530

    a

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
-
+ 
 //create a post
 
 router.post("/", async (req, res) => {
